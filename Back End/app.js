9261b2e1d8fdd0f1c30cacdad52aa48a6e14b3d3
Suraj MS <surajmshivakumar@gmail.com>
2024-11-17 16:59:55 +0530

    cor

--- Back End/app.js
+++ Back End/app.js
@@ -21,11 +21,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
-app.use(cors({
-    origin: 'https://shivajicreationfullstack.onrender.com/api/v1',
-    credentials: true,
-}));
 
 
 //morgan middleware
